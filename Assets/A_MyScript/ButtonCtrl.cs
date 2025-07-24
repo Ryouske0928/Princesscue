1fe0fcf19b4159aa0361904ba042a1d6845d7d06
Royzn <yamiryo22@gmail.com>
2025-07-24 12:29:35 +0900

    タイトル画面のボタン操作と選択時にハイライトするスクリプト追加

---
+++ Assets/A_MyScript/ButtonCtrl.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonCtrl : MonoBehaviour
+{
+    [SerializeField] Button[] buttons; //インスペクター上で操作したいボタン設定
+    private int _selectButtonNum = -1;
+    private bool firstSelect = false;  //初回の入力確認フラグ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)　|| Input.GetKeyDown(KeyCode.S))
+        {
+            if(!firstSelect)
+            {
+                _selectButtonNum = 0; //初回入力で0番目のボタンになるように設定
+                firstSelect = true;
+            }
+
+            else
+            {
+                //Wキー入力で上へ移動
+                if(Input.GetKeyDown(KeyCode.W))
+                {
+                    _selectButtonNum--;
+                    if(_selectButtonNum < 0)
+                    {
+                        _selectButtonNum = buttons.Length - 1;
+                    }
+                }
+                //Sキー入力で下へ移動
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    _selectButtonNum++;
+                    if(_selectButtonNum >= buttons.Length)
+                    {
+                        _selectButtonNum = 0;
+                    }
+                }
+            }
+
+            SelectButton(_selectButtonNum);  //ボタンを選択状態にする
+        }
+
+        //Enterキーで決定
+        if(firstSelect && Input.GetKeyDown(KeyCode.Return))
+        {
+            buttons[_selectButtonNum].onClick.Invoke();
+        }
+    }
+
+    private void SelectButton(int buttonNum)
+    {
+        EventSystem.current.SetSelectedGameObject(buttons[buttonNum].gameObject);  //イベントシステムに選択されたボタンを設定
+    }
+}
---
+++ Assets/A_MyScript/ButtonOutline.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonOutline : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] Outline buttonOutline;  // Buttonに設定したOutlineコンポーネントをアタッチ
+    [SerializeField] Color selectedColor = Color.blue;
+    private Color normalColor = Color.clear;
+
+    // ボタンが選択された時
+    public void OnSelect(BaseEventData eventData)
+    {
+        buttonOutline.effectColor = selectedColor;  //選択された時に輪郭の色を任意の色に変更する　(初期は青に設定)
+    }
+
+    // ボタンが選択解除された時
+    public void OnDeselect(BaseEventData eventData)
+    {
+        buttonOutline.effectColor = normalColor;  // 通常の輪郭は透明
+    }
+
+    //ボタンにマウスカーソル重なったとき時
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonOutline.effectColor = selectedColor;  //選択された時に輪郭の色を任意の色に変更する　(初期は青)
+    }
+
+    //ボタンからマウスカーソルが外れた時
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonOutline.effectColor = normalColor;  // 通常の輪郭は透明
+    }
+}
