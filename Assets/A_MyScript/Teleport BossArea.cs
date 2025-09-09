fd42b7c7a266b369fbd6143e88624ae6547586b3
Royzn <yamiryo22@gmail.com>
2025-09-10 01:25:16 +0900

    ラスボス扉前のワープ処理追加

---
+++ Assets/A_MyScript/Teleport BossArea.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportBossArea : MonoBehaviour
+{
+    [SerializeField] Transform BossZone;  //ボスエリアへのワープ先座標（扉前）
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CharacterController cc = other.GetComponent<CharacterController>();
+            cc.enabled = false;
+            other.transform.position = BossZone.position;
+            cc.enabled = true;
+        }
+    }
+}
