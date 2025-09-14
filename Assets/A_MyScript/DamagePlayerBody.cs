3226a0f2752d6d7dbd51ee77f751bfcea4532366
Royzn <yamiryo22@gmail.com>
2025-09-14 16:07:56 +0900

    Stage2ボス追加に合わせてダメージ判定修正＆ステージ２ボス配置

--- Assets/A_MyScript/DamagePlayerBody.cs
+++ Assets/A_MyScript/DamagePlayerBody.cs
@@ -8,6 +8,8 @@ public class DamagePlayerBody : MonoBehaviour
     private string EnemyTag = "EnemyWeapon";　//敵のタグ
     private string BossTag1 = "BossWeapon1";　　　　　　　　　 //ボスと処理分けしたくなった時ように用意
     private string BossTag2 = "BossWeapon2";
+    private string Boss2Tag1 = "Boss2Weapon1";
+    private string Boss2Tag2 = "Boss2Weapon2";
     public bool hasHitP = false;
     private void OnTriggerEnter(Collider other)
     {
@@ -27,6 +29,19 @@ public class DamagePlayerBody : MonoBehaviour
         if (!hasHitP && other.tag == BossTag2)
         {
             BossCtrl bossCtrl = other.GetComponentInParent<BossCtrl>();
+            health.TakeDamage(bossCtrl._bossATK2);
+            hasHitP = true;
+        }
+
+        if (!hasHitP && other.tag == Boss2Tag1)
+        {
+            Stage2Boss bossCtrl = other.GetComponentInParent<Stage2Boss>();
+            health.TakeDamage(bossCtrl._bossATK1);
+            hasHitP = true;
+        }
+        if (!hasHitP && other.tag == Boss2Tag2)
+        {
+            Stage2Boss bossCtrl = other.GetComponentInParent<Stage2Boss>();
             health.TakeDamage(bossCtrl._bossATK2);
             hasHitP = true;
         }
